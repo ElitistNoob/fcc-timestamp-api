33b1e139acdb085ad9f234d67dd9f2a9c167111f
Kevin Hernot <kevin@labs.haystack.asia>
2023-11-19 13:32:36 +0700

    accounted for invalid date and empty params

--- app.js
+++ app.js
@@ -12,16 +12,33 @@ app.get('/', (req, res) => {
     res.sendFile(absolutePath);
 })
 
-app.get('/api/:inputtedDate',  (req, res) => {
-    const { inputtedDate } = req.params
-    const dateFormat = isNaN(inputtedDate) ? inputtedDate : parseInt(inputtedDate)
-    const date = new Date(dateFormat);
-    res.json({
-        'unix': date.getTime(),
-        'utc': date.toUTCString()
-    })
+function isValidDate(str) {
+    const parsedDate = new Date(str);
+    return parsedDate.toString() !== 'Invalid Date'
+}
+
+app.get('/api/timestamp/:date?',  (req, res) => {
+    const { date } = req.params
+    const dateFormat = isNaN(date) ? date : parseInt(date);
+    let dateQuery = new Date(dateFormat);
+
+    if (isValidDate(dateFormat) || !req.params.date) {
+        if (!req.params.date) {
+            dateQuery = new Date();
+        }
+
+        res.json({
+            'unix': dateQuery.getTime(),
+            'utc': dateQuery.toUTCString()
+        })
+    } else {
+        res.json({
+            "error": "Invalid Date"
+        })
+    }
 })
 
 const listener = app.listen(process.env.PORT,() => {
     console.log({port: listener.address().port})
 })
+
