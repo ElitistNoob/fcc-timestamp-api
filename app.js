08b7012371e336e5dfc19a2315a936e38995060b
Kevin Hernot <kevin@labs.haystack.asia>
2023-11-19 10:54:27 +0700

    initial commit

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-const fullDate = require('./utilities/date')
 const express = require('express');
+const cors = require('cors')
+
 const app = express();
 
-const absolutePath = `${__dirname}/index.html`
-const { getDateString, getTime } = fullDate
-
-const cors = require('cors')
+const absolutePath = `${__dirname}/views/index.html`
 
 app.use(cors({optionsSuccessStatus: 200}));
 app.use(express.static('public'));
@@ -14,15 +12,14 @@ app.get('/', (req, res) => {
     res.sendFile(absolutePath);
 })
 
-app.get('api/:inputtedDate',  (req, res) => {
+app.get('/api/:inputtedDate',  (req, res) => {
     const { inputtedDate } = req.params
-    let data = inputtedDate;
-
-    if (!data.split('').some((x) => x === '-')) {
-       data = parseInt(data);
-    }
-
-    res.json({'unix': getTime(data), 'utc': getDateString(data)})
+    const dateFormat = isNaN(inputtedDate) ? inputtedDate : parseInt(inputtedDate)
+    const date = new Date(dateFormat);
+    res.json({
+        'unix': date.getTime(),
+        'utc': date.toUTCString()
+    })
 })
 
 const listener = app.listen(process.env.PORT,() => {
